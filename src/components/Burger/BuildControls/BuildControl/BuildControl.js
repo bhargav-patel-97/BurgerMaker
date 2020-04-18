8530282d6f08ed603096ea8dd6cf54d8ca7c14c5
bhargav <bhargavpatel6040@gmail.com>
2020-04-18 21:20:35 +0530

    Subject: BurgerMaker//dynamic-builder feature implemented Message: Added dynamic controls and required functionalities. Next step: Moving on to next branch for implementing checkout feature.

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -4,8 +4,13 @@ import styles from './BuildControl.css'
 const buildControl = (props) => (
     <div className={styles.BuildControl}>
         <div className={styles.Label}>{props.label}</div>
-        <button className={styles.Less}>Less</button>
-        <button className={styles.More}>More</button>
+        <button 
+            className={styles.Less} 
+            onClick={props.remove} 
+            disabled={props.disabled}>Less</button>
+        <button 
+            className={styles.More} 
+            onClick={props.add}>More</button>
     </div>
 );
 
